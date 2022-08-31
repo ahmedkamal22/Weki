c84d0f3c82a329cf0768a09a3d727091a21bc811
ahmedkamal22 <ak0850000@gmail.com>
2022-08-31 23:34:04 +0200

    In this branch I built home layout

---
+++ lib/modules/settings/settings.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:weki/shared/components/components.dart';
+
+class SettingsScreen extends StatelessWidget {
+  const SettingsScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return testScreen(text: "SettingsScreen");
+  }
+}
