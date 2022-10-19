3d6d7d9b97f631833141afa1e085d908966f4a9c
ahmedkamal22 <ak0850000@gmail.com>
2022-10-19 08:04:36 +0200

    Here I made hot-fix on posts and chats

--- lib/modules/posts/posts.dart
+++ lib/modules/posts/posts.dart
@@ -48,177 +48,156 @@ class PostsScreen extends StatelessWidget {
             padding: const EdgeInsets.all(20.0),
             child: Form(
               key: formKey,
-              child: Expanded(
-                child: Column(
-                  children: [
-                    if (state is AppCreatePostLoadingState)
-                      Column(
-                        children: [
-                          LinearProgressIndicator(),
-                          SizedBox(
-                            height: 10,
-                          ),
-                        ],
-                      ),
-                    Row(
+              child: Column(
+                children: [
+                  if (state is AppCreatePostLoadingState)
+                    Column(
                       children: [
-                        CircleAvatar(
-                          radius: 30,
-                          backgroundImage:
-                              NetworkImage("${cubit.userModel!.image}"),
-                        ),
+                        LinearProgressIndicator(),
                         SizedBox(
-                          width: 15,
-                        ),
-                        Expanded(
-                          child: Text(
-                            "${cubit.userModel!.name}",
-                            style:
-                                Theme.of(context).textTheme.bodyText1!.copyWith(
-                                      fontSize: 18,
-                                      color: AppCubit.get(context).isDark
-                                          ? Colors.white
-                                          : Colors.black,
-                                    ),
-                          ),
+                          height: 10,
                         ),
                       ],
                     ),
-                    SizedBox(
-                      height: 10,
-                    ),
-                    Expanded(
-                      child: TextFormField(
-                        style: TextStyle(
+                  Row(
+                    children: [
+                      CircleAvatar(
+                        radius: 30,
+                        backgroundImage:
+                            NetworkImage("${cubit.userModel!.image}"),
+                      ),
+                      SizedBox(
+                        width: 15,
+                      ),
+                      Expanded(
+                        child: Text(
+                          "${cubit.userModel!.name}",
+                          style:
+                              Theme.of(context).textTheme.bodyText1!.copyWith(
+                                    fontSize: 18,
+                                    color: AppCubit.get(context).isDark
+                                        ? Colors.white
+                                        : Colors.black,
+                                  ),
+                        ),
+                      ),
+                    ],
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Expanded(
+                    child: TextFormField(
+                      style: TextStyle(
+                        color: AppCubit.get(context).isDark
+                            ? Colors.white
+                            : Colors.black,
+                      ),
+                      controller: postTextController,
+                      autocorrect: true,
+                      validator: (value) {
+                        if (value!.isEmpty) {
+                          return "This field can't be null";
+                        }
+                        return null;
+                      },
+                      onFieldSubmitted: (value) {
+                        if (formKey.currentState!.validate()) {
+                          if (cubit.postImage == null) {
+                            cubit.createPost(
+                                postData: DateTime.now().toString(),
+                                postText: postTextController.text);
+                          } else {
+                            cubit.uploadPost(
+                                postData: DateTime.now().toString(),
+                                postText: postTextController.text);
+                            cubit.removePostImage();
+                          }
+                          postTextController.clear();
+                          Navigator.pop(context);
+                        }
+                      },
+                      decoration: InputDecoration(
+                        errorStyle: TextStyle(
+                            fontWeight: FontWeight.bold, fontSize: 16),
+                        hintText: "What's on your mind\t?",
+                        border: InputBorder.none,
+                        hintStyle: TextStyle(
                           color: AppCubit.get(context).isDark
                               ? Colors.white
                               : Colors.black,
                         ),
-                        controller: postTextController,
-                        autocorrect: true,
-                        validator: (value) {
-                          if (value!.isEmpty) {
-                            return "This field can't be null";
-                          }
-                          return null;
-                        },
-                        onFieldSubmitted: (value) {
-                          if (formKey.currentState!.validate()) {
-                            if (cubit.postImage == null) {
-                              cubit.createPost(
-                                  postData: DateTime.now().toString(),
-                                  postText: postTextController.text);
-                            } else {
-                              cubit.uploadPost(
-                                  postData: DateTime.now().toString(),
-                                  postText: postTextController.text);
-                              cubit.removePostImage();
-                            }
-                            postTextController.clear();
-                            Navigator.pop(context);
-                          }
-                        },
-                        decoration: InputDecoration(
-                          errorStyle: TextStyle(
-                              fontWeight: FontWeight.bold, fontSize: 16),
-                          hintText: "What's on your mind\t?",
-                          border: InputBorder.none,
-                          hintStyle: TextStyle(
-                            color: AppCubit.get(context).isDark
-                                ? Colors.white
-                                : Colors.black,
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  if (cubit.postImage != null)
+                    Stack(
+                      alignment: AlignmentDirectional.topEnd,
+                      children: [
+                        Container(
+                          height: 140,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(4.0),
+                            image: DecorationImage(
+                                image: FileImage(cubit.postImage!),
+                                fit: BoxFit.cover),
                           ),
                         ),
-                      ),
-                    ),
-                    SizedBox(
-                      height: 10,
-                    ),
-                    if (cubit.postImage != null)
-                      Stack(
-                        alignment: AlignmentDirectional.topEnd,
-                        children: [
-                          Container(
-                            height: 140,
-                            decoration: BoxDecoration(
-                              borderRadius: BorderRadius.circular(4.0),
-                              image: DecorationImage(
-                                  image: FileImage(cubit.postImage!),
-                                  fit: BoxFit.cover),
+                        Padding(
+                          padding:
+                              const EdgeInsetsDirectional.only(top: 5, end: 5),
+                          child: CircleAvatar(
+                            radius: 16,
+                            backgroundColor: Colors.blue,
+                            child: IconButton(
+                              iconSize: 17,
+                              icon: Icon(Icons.close),
+                              onPressed: () {
+                                cubit.removePostImage();
+                              },
                             ),
                           ),
-                          Padding(
-                            padding: const EdgeInsetsDirectional.only(
-                                top: 5, end: 5),
-                            child: CircleAvatar(
-                              radius: 16,
-                              backgroundColor: Colors.blue,
-                              child: IconButton(
-                                iconSize: 17,
-                                icon: Icon(Icons.close),
-                                onPressed: () {
-                                  cubit.removePostImage();
-                                },
-                              ),
-                            ),
-                          ),
-                        ],
-                      ),
-                    SizedBox(
-                      height: 30,
+                        ),
+                      ],
                     ),
-                    Padding(
-                      padding: const EdgeInsets.all(10.0),
-                      child: Row(
-                        children: [
-                          Padding(
-                            padding:
-                                const EdgeInsetsDirectional.only(start: 30),
-                            child: Expanded(
-                              child: InkWell(
-                                onTap: () {
-                                  cubit.getPostImage();
-                                },
-                                child: Padding(
-                                  padding: const EdgeInsets.all(10.0),
-                                  child: Row(
-                                    mainAxisAlignment: MainAxisAlignment.center,
-                                    children: [
-                                      Icon(IconBroken.Image,
-                                          color: Colors.blue),
-                                      SizedBox(
-                                        width: 5,
-                                      ),
-                                      Text(
-                                        "add photo",
-                                        style: TextStyle(
-                                            fontSize: 16, color: Colors.blue),
-                                      ),
-                                    ],
+                  SizedBox(
+                    height: 30,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(10.0),
+                    child: Row(
+                      children: [
+                        Expanded(
+                          child: InkWell(
+                            onTap: () {
+                              cubit.getPostImage();
+                            },
+                            child: Padding(
+                              padding: const EdgeInsets.all(10.0),
+                              child: Row(
+                                // mainAxisAlignment: MainAxisAlignment.end,
+                                children: [
+                                  Icon(IconBroken.Image, color: Colors.blue),
+                                  SizedBox(
+                                    width: 5,
                                   ),
-                                ),
+                                  Text(
+                                    "add photo",
+                                    style: TextStyle(
+                                        fontSize: 16, color: Colors.blue),
+                                  ),
+                                ],
                               ),
                             ),
                           ),
-                          Spacer(),
-                          Expanded(
-                            child: InkWell(
-                              onTap: () {},
-                              child: Padding(
-                                padding: const EdgeInsets.all(10.0),
-                                child: Text(
-                                  "# tags",
-                                  style: TextStyle(
-                                      fontSize: 16, color: Colors.blue),
-                                ),
-                              ),
-                            ),
-                          ),
-                        ],
-                      ),
+                        ),
+                      ],
                     ),
-                  ],
-                ),
+                  ),
+                ],
               ),
             ),
           ),
